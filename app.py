96951dc2ca798e163190724cf0210e89756ea127
Lanieza <laniezalopez16@gmail.com>
2025-05-17 14:32:05 +0800

    fixed some streamlit deployment errors

--- app.py
+++ app.py
@@ -1,38 +1,35 @@
 import streamlit as st
 import numpy as np
-import cv2
 from tensorflow.keras.models import load_model
 from PIL import Image
 
 # Load model
 model = load_model('drowsiness_eye_model.h5')
 
-# Page title
 st.title("Drowsiness Detection (Eye Open/Closed)")
-st.write("Upload an image of your eyes OR take a photo using your webcam.")
+st.write("Upload or capture an image of your eyes to check if they're open or closed.")
 
-# Let user choose input method
-option = st.radio("Choose input method:", ("Upload Image", "Use Webcam"))
+# Option 1: Capture using camera
+img_file_buffer = st.camera_input("📷 Take a photo using your webcam or phone")
 
-image = None
+# Option 2: Upload an image
+uploaded_file = st.file_uploader("📁 Or upload an image...", type=["jpg", "jpeg", "png"])
 
-if option == "Upload Image":
-    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
-    if uploaded_file is not None:
-        image = Image.open(uploaded_file)
-elif option == "Use Webcam":
-    picture = st.camera_input("Take a picture")
-    if picture:
-        image = Image.open(picture)
+# Use camera input if available, otherwise use uploaded image
+if img_file_buffer is not None:
+    image = Image.open(img_file_buffer).convert("L")
+elif uploaded_file is not None:
+    image = Image.open(uploaded_file).convert("L")
+else:
+    image = None
 
 if image is not None:
     st.image(image, caption="Input Image", use_column_width=True)
 
-    # Convert image to grayscale and resize to 24x24
-    img = np.array(image.convert('L'))  # grayscale
-    img = cv2.resize(img, (24, 24))     # resize to 24x24
-    img = img / 255.0                   # normalize
-    img = img.reshape(1, 24, 24, 1)     # reshape for model
+    # Resize and preprocess
+    image = image.resize((24, 24))
+    img = np.array(image) / 255.0
+    img = img.reshape(1, 24, 24, 1)
 
     # Predict
     prediction = model.predict(img)[0][0]
